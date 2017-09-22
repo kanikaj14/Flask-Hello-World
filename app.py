f532b6a222601527ed625b9d8afded9bb10f2e60
SDCORP\39785 <kanika.jain@sandisk.com>
2017-09-22 19:42:28 +0530

    bla bla

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "<h1>Hello heroku</h1>"
+    return ("bla bla bla bla!")
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+if __name__ == "__main__":
+    app.run()
